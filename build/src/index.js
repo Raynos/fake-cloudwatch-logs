0f4baa0387782cff32a29323c006e5b0cecb3bc7
Raynos <raynos2@gmail.com>
2019-11-24 10:05:22 +0100

    build: generated files

--- build/src/index.js
+++ build/src/index.js
@@ -2,11 +2,100 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const http = require("http");
 const util = require("util");
+const path = require("path");
+const fs = require("fs");
+const mkdirP = util.promisify(fs.mkdir);
+const writeFileP = util.promisify(fs.writeFile);
+const readFileP = util.promisify(fs.readFile);
+const readdirP = util.promisify(fs.readdir);
 class FakeCloudwatchLogs {
     constructor(options = {}) {
         this.httpServer = http.createServer();
         this.port = options.port || 0;
         this.hostPort = null;
+        this.touchedCache = false;
+        this.knownCaches = [];
+        this.rawGroups = [];
+        this.rawStreams = {};
+        this.rawEvents = {};
+        this.tokens = {};
+    }
+    async cacheGroupsToDisk(filePath, groups) {
+        this.touchedCache = true;
+        if (!this.knownCaches.includes(filePath)) {
+            this.knownCaches.push(filePath);
+        }
+        await mkdirP(filePath);
+        await writeFileP(path.join(filePath, 'groups.json'), JSON.stringify({
+            type: 'cached-log-group',
+            groups
+        }), 'utf8');
+    }
+    async cacheStreamsToDisk(filePath, groupName, streams) {
+        this.touchedCache = true;
+        if (!this.knownCaches.includes(filePath)) {
+            this.knownCaches.push(filePath);
+        }
+        await mkdirP(filePath);
+        await mkdirP(path.join(filePath, 'groups'));
+        await mkdirP(path.join(filePath, 'groups', groupName));
+        await writeFileP(path.join(filePath, 'groups', groupName, 'streams.json'), JSON.stringify({
+            type: 'cached-log-stream',
+            groupName,
+            streams
+        }));
+    }
+    async populateFromCache(filePath) {
+        let groupsStr = null;
+        try {
+            groupsStr = await readFileP(path.join(filePath, 'groups.json'), 'utf8');
+        }
+        catch (maybeErr) {
+            const err = maybeErr;
+            if (err.code !== 'ENOENT')
+                throw err;
+        }
+        if (groupsStr) {
+            const groupsInfo = JSON.parse(groupsStr);
+            this.populateGroups(groupsInfo.groups);
+        }
+        let groupDirs = null;
+        try {
+            groupDirs = await readdirP(path.join(filePath, 'groups'));
+        }
+        catch (maybeErr) {
+            const err = maybeErr;
+            if (err.code !== 'ENOENT')
+                throw err;
+        }
+        if (groupDirs) {
+            for (const groupName of groupDirs) {
+                const streamsStr = await readFileP(path.join(filePath, 'groups', groupName, 'streams.json'), 'utf8');
+                const streamsInfo = JSON.parse(streamsStr);
+                this.populateStreams(streamsInfo.groupName, streamsInfo.streams);
+            }
+        }
+    }
+    populateGroups(groups) {
+        this.rawGroups.push(...groups);
+    }
+    populateStreams(groupName, streams) {
+        let rawStreams = this.rawStreams[groupName];
+        if (rawStreams === undefined) {
+            rawStreams = this.rawStreams[groupName] = [];
+        }
+        rawStreams.push(...streams);
+    }
+    populateEvents(groupName, streamName, events) {
+        let eventsByGroup = this.rawEvents[groupName];
+        if (eventsByGroup === undefined) {
+            eventsByGroup = this.rawEvents[groupName] = {};
+        }
+        let eventsByStream = eventsByGroup[streamName];
+        if (!eventsByStream) {
+            eventsByStream = eventsByGroup[streamName] = [];
+        }
+        eventsByStream.push(...events);
     }
     async bootstrap() {
         if (!this.httpServer) {
@@ -46,14 +135,20 @@ class FakeCloudwatchLogs {
             const lastPart = parts[parts.length - 1];
             let respBody;
             switch (lastPart) {
+                case 'DescribeLogGroups':
+                    respBody = this.describeLogGroups(body);
+                    break;
                 case 'DescribeLogStreams':
                     respBody = this.describeLogStreams(body);
+                    break;
+                case 'GetLogEvents':
+                    respBody = this.getLogEvents(body);
                     break;
                 default:
                     break;
             }
-            if (!respBody) {
-                res.statusCode = 400;
+            if (typeof respBody !== 'object') {
+                res.statusCode = 404;
                 res.end('Not Found');
                 return;
             }
@@ -63,8 +158,85 @@ class FakeCloudwatchLogs {
             res.end(JSON.stringify(respBody));
         });
     }
+    paginate(rawItems, prevToken, limit) {
+        let offset = 0;
+        if (prevToken) {
+            const tokenInfo = this.tokens[prevToken];
+            // tslint:disable-next-line: no-dynamic-delete
+            delete this.tokens[prevToken];
+            if (!tokenInfo) {
+                throw new Error(`invalid prevToken: ${prevToken}`);
+            }
+            offset = tokenInfo.offset;
+        }
+        const end = offset + (limit || 50);
+        const items = rawItems.slice(offset, end);
+        let nextToken;
+        if (rawItems.length > end) {
+            nextToken = cuuid();
+            this.tokens[nextToken] = { offset: end };
+        }
+        return { items, nextToken };
+    }
+    describeLogGroups(body) {
+        const req = JSON.parse(body);
+        // TODO: req.logGroupNamePrefix
+        // TODO: default sort
+        const page = this.paginate(this.rawGroups, req.nextToken, req.limit);
+        // tslint:disable-next-line: no-unnecessary-local-variable
+        const res = {
+            logGroups: page.items,
+            nextToken: page.nextToken
+        };
+        return res;
+    }
     describeLogStreams(body) {
+        const req = JSON.parse(body);
+        // TODO: default sort
+        // TODO: req.logStreamNamePrefix
+        // TODO: req.descending
+        // TODO: req.orderBy
+        const streamsByGroup = this.rawStreams[req.logGroupName];
+        if (!streamsByGroup) {
+            return {};
+        }
+        const page = this.paginate(streamsByGroup, req.nextToken, req.limit);
+        // tslint:disable-next-line: no-unnecessary-local-variable
+        const res = {
+            logStreams: page.items,
+            nextToken: page.nextToken
+        };
+        return res;
+    }
+    getLogEvents(body) {
+        const req = JSON.parse(body);
+        // TODO: req.startTime
+        // TODO: req.endTime
+        // TODO: req.startFromHead
+        const eventsByGroup = this.rawEvents[req.logGroupName];
+        if (!eventsByGroup) {
+            return {};
+        }
+        const events = eventsByGroup[req.logStreamName];
+        if (!events) {
+            return {};
+        }
+        // tslint:disable-next-line: no-unnecessary-local-variable
+        const res = {
+            events
+        };
+        return res;
     }
 }
 exports.FakeCloudwatchLogs = FakeCloudwatchLogs;
+function cuuid() {
+    const str = (Date.now().toString(16) +
+        // tslint:disable-next-line: insecure-random
+        Math.random().toString(16).slice(2) +
+        // tslint:disable-next-line: insecure-random
+        Math.random().toString(16).slice(2)).slice(0, 32);
+    return str.slice(0, 8) + '-' + str.slice(8, 12) + '-' +
+        str.slice(12, 16) + '-' + str.slice(16, 20) + '-' +
+        str.slice(20);
+}
 //# sourceMappingURL=index.js.map
--- build/test/test-harness.js
+++ build/test/test-harness.js
@@ -1,2 +1,45 @@
 'use strict';
+Object.defineProperty(exports, "__esModule", { value: true });
+const AWS = require("aws-sdk");
+const tape = require("tape");
+const rimraf = require("rimraf");
+const util = require("util");
+const tapeCluster = require("tape-cluster");
+const index_1 = require("../src/index");
+class TestHarness {
+    constructor() {
+        this.cwServer = new index_1.FakeCloudwatchLogs({
+            port: 0
+        });
+        this.cw = null;
+    }
+    async bootstrap() {
+        const hostPort = await this.cwServer.bootstrap();
+        this.cw = new AWS.CloudWatchLogs({
+            region: 'us-east-1',
+            endpoint: `http://${hostPort}`,
+            sslEnabled: false,
+            accessKeyId: '123',
+            secretAccessKey: 'abc'
+        });
+    }
+    getServer() {
+        return this.cwServer;
+    }
+    getCW() {
+        if (!this.cw)
+            throw new Error('not bootstrapped yet');
+        return this.cw;
+    }
+    async close() {
+        await this.cwServer.close();
+        for (const cachePath of this.cwServer.knownCaches) {
+            await util.promisify((cb) => {
+                rimraf(cachePath, cb);
+            })();
+        }
+    }
+}
+exports.TestHarness = TestHarness;
+exports.test = tapeCluster(tape, TestHarness);
 //# sourceMappingURL=test-harness.js.map
